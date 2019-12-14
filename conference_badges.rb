e75fa098fa95aa3423fd85a477a977386bdaac82
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-14 21:56:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
   
 def batch_badge_creator(array)
   new_array = []
- array.each {|name|new_array.push("Hello, my name is #{name}.")
+ array.each {|name|new_array.push("Hello, my name is #{name}.")}
 
   end
  end
