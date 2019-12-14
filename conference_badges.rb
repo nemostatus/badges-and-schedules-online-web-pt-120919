90b8694acddc5c6d119069766fb643a899604d16
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-14 21:54:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+  end
+  badge_maker("Arel")
+  
+def batch_badge_creator(array)
+ array.each do |name|
+   puts "Hello, my name is #{name}."
+  end
+ end
+batch_badge_creator(["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus",  "Matz"])
+  
